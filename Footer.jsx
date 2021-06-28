7beb144323940f0f5951f20f96e41177709a677e
SajadSaj <86597507+SajadSaj@users.noreply.github.com>
2021-06-28 14:40:11 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer(){
+  return(
+   <footer>
+      <p>
+        Copryright by ShapeAI @ {new Date().getFullYear()}
+      </p> 
+   </footer>
+     
+  );
+
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>JavaScript and React JS</h1> 
+      <p>Basic web dev React js bootcamp</p>
+    </div>
+     
+  );
+
+}
+
+export default Info;
